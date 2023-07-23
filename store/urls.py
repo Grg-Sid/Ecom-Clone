5640d7afe03960c554a9fe88701189fa26d7587e
Grg-Sid <siddhantgurung007@gmail.com>
2023-07-23 16:01:47 +0530

    Initial Commit

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, reverse
+from . import views
+
+# URL Routes
+urlpatterns = [
+    path("products/", views.product_list),
+    path("products/<int:id>", views.product_detail),
+    path("collections/<int:pk>", views.collection_detail, name="collection-detail"),
+]
---
+++ store/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializers
+
+
+# Create your views here.
+@api_view()
+def product_list(request):
+    queryset = Product.objects.select_related("collection").all()
+    serializer = ProductSerializers(queryset, many=True, context={"request": request})
+    return Response(serializer.data)
+
+
+@api_view()
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializers(product)
+    return Response(serializer.data)
+
+ 
+@api_view()
+def collection_detail(request, pk):
+    return Response("ok")
